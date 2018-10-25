6b3472b75de9bd234fb3997eb708433a7f3f0147
Tyson Grogan <tyson31088@gmail.com>
2018-10-24 21:21:50 -0400

    Prints average wine score

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 wine_df = pd.read_csv("C:\\Users\\Tyson\\PycharmProjects\\StatsInterpreter\\data\\winemag-data-130k-v2.csv")
 
-print(wine_df["country"][16])
+print(wine_df.points.mean())
