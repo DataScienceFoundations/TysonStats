cedc9eb521664a47d6f22247bc161d86c07dcd14
Tyson Grogan <tyson31088@gmail.com>
2018-10-28 14:44:19 -0400

    Now doing scotch

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import pandas as pd
+import seaborn as sb
+from io import StringIO
 
-wine_df = pd.read_csv("C:\\Users\\Tyson\\PycharmProjects\\StatsInterpreter\\data\\winemag-data-130k-v2.csv")
+scotch_df = pd.read_csv("C:\\Users\\Tyson\\PycharmProjects\\StatsInterpreter\\data\\scotch_review.csv")
 
-print(wine_df.points.mean())
+print(scotch_df.score.mean())
+print(scotch_df.score.value_counts(sort=False))
+print(scotch_df.price.value_counts(sort=False))
+
+
+cleaned_df = scotch_df[scotch_df.price.apply(lambda x: x.isnumeric())]
+
+print(cleaned_df.score.value_counts(sort=False))
+print(cleaned_df.price.value_counts(sort=False))
+
+cleaned_df = cleaned_df.sort_values(by=['price'])
+
+sb.regplot('price', 'score', data=cleaned_df)
