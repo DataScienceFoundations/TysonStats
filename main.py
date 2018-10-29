5aaec1be01866978467e096e5dea9cc56044fcf9
Tyson Grogan <tyson31088@gmail.com>
2018-10-28 21:47:35 -0400

    Changed to monitor coffee sales over time for a bakery

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pandas as pd
-import seaborn as sb
-from io import StringIO
+import seaborn as sns
 
-scotch_df = pd.read_csv("C:\\Users\\Tyson\\PycharmProjects\\StatsInterpreter\\data\\scotch_review.csv")
+bakery_df = pd.read_csv("C:\\Users\\Tyson\\PycharmProjects\\StatsInterpreter\\data\\BreadBasket_DMS.csv")
 
-print(scotch_df.score.mean())
-print(scotch_df.score.value_counts(sort=False))
-print(scotch_df.price.value_counts(sort=False))
+print(bakery_df.Item.value_counts(sort=False))
 
+hot_chocolate_sales = bakery_df.loc[bakery_df['Item']=="Coffee"]
 
-cleaned_df = scotch_df[scotch_df.price.apply(lambda x: x.isnumeric())]
+print(hot_chocolate_sales.Item.value_counts(sort=False))
 
-print(cleaned_df.score.value_counts(sort=False))
-print(cleaned_df.price.value_counts(sort=False))
+hot_chocolate_daily = hot_chocolate_sales.Date.value_counts().sort_index()
 
-cleaned_df = cleaned_df.sort_values(by=['price'])
+print(hot_chocolate_daily)
 
-sb.regplot('price', 'score', data=cleaned_df)
+date_index = pd.date_range('10-30-2016', '04-09-2017')
+
+hot_chocolate_daily.index = pd.DatetimeIndex(hot_chocolate_daily.index)
+
+hot_chocolate_daily = hot_chocolate_daily.reindex(date_index, fill_value=0)
+
+ax_daily = hot_chocolate_daily.plot()
+
+rolling = hot_chocolate_daily.rolling(7, min_periods=1).mean()
+
+rolling.plot(ax=ax_daily)
